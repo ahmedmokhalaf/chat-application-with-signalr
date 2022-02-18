c3356aa5d47734cb23fd674238fbfa8962e85979
AhmedKhalef <ahmedmkhalef@gmail.com>
2022-02-18 16:39:49 +0200

    Initi App

---
+++ ChatApp.Hubs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChatApp.Hubs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
